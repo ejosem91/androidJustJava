aacbd5fa10002837c9f7e77df62e13d4306040df
green91 <e.josem91@gmail.com>
2016-10-12 12:35:52 -0600

    String and Traslate

--- app/src/main/java/com/example/lenovo/androidjustjava/MainActivity.java
+++ app/src/main/java/com/example/lenovo/androidjustjava/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.lenovo.androidjustjava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     int orderNum=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,70 +23,93 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void incrementOrder(View v){
-        orderNum = orderNum +1;
-        displayQuantity(orderNum);
+        if(orderNum == 100){
+            Toast.makeText(this, "you can't get more than 100 cups", Toast.LENGTH_SHORT).show();
+        }
+        if(orderNum < 100){
+            orderNum = orderNum +1;
+            displayQuantity(orderNum);
+        }
     }
     public void decrementOrder(View v){
 
-        if(orderNum>0){
+        if(orderNum == 1){
+            Toast.makeText(this, "can not get less than 1 cup", Toast.LENGTH_SHORT).show();
+        }
+        if(orderNum>1){
             orderNum = orderNum -1;
             displayQuantity(orderNum);
+
         }
 
     }
 
     public void submitOrder(View v){
-        int orderPrice= calculatePrice();
-        String summary =createOrderSummary();
-        PrintTotal(summary);
-    }
-
-    private String createOrderSummary(){
-        String summary;
         CheckBox ch = (CheckBox) findViewById(R.id.whipped);
         boolean hasWhipped = ch.isChecked();
         CheckBox cho = (CheckBox) findViewById(R.id.chocolate);
         boolean hasChocolate = cho.isChecked();
+
         EditText editTextName = (EditText) findViewById(R.id.name_text);
         String name = editTextName.getText().toString();
 
 
 
-        if(hasWhipped == true && hasChocolate ==true){
-            int orderPrice= calculatePrice();
-            summary ="Name:" + name +
-                    "\nQuantity "+ orderNum +
-                    "\nwe want Chocolate ?? " + hasChocolate+
-                    "\nWe want Whipped ?? " + hasWhipped+
-                    "\nTotal $: "+ orderPrice +
-                    "\nThank you!!";
-            return summary ;
-        }else{
-            int orderPrice= calculatePrice();
-            summary = "Name: " + name +
-                    "\nQuantity "+ orderNum +
-                    "\nwe want Chocolate ?? " + hasChocolate +
-                    "\nwe want Whipped ?? " + hasWhipped +
-                    "\nTotal $: "+ orderPrice +
-                    "\nThank you!!";
-            return summary ;
+        int orderPrice= calculatePrice(hasWhipped,hasChocolate);
+
+        String priceMessage = createOrderSumary(name,orderPrice,hasWhipped,hasChocolate);
+
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, "e.josem91@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Cooffe");
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
         }
 
+
     }
 
-    private int calculatePrice(){
-        int price =5 * orderNum;
-        return price;
+    private int calculatePrice( boolean addWhipperCream, boolean addChocolate){
+        int price =5;
+
+
+        if (addWhipperCream){
+            price = price + 1;
+        }
+
+        if(addChocolate){
+            price = price +2;
+        }
+
+       // if(addChocolate && addWhipperCream){
+         //   price = price + 3;
+        //}
+
+        return price * orderNum;
+    }
+
+
+    private String createOrderSumary(String name, int price,boolean hasWhipper, boolean hasChocolate){
+        String summary;
+
+        summary = "\nName: " + name ;
+        summary +="\nPrice $: " + price;
+        summary +="\nWe have Whipper ?? " + hasWhipper;
+        summary +="\nWe have Chocolate ?? " + hasChocolate;
+        summary +="\n" + getString( R.string.thank_you);
+
+
+
+        return summary;
     }
 
 
 
 
-    private void PrintTotal(String total){
 
-        TextView t = (TextView) findViewById(R.id.total);
-        t.setText(" " + total);
-    }
 
     private void displayQuantity(int  numberOfCoffees){
         TextView t = (TextView) findViewById(R.id.orderNum);
